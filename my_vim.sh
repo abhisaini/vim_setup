323698bd136651c178bba568faecefdd7ae03ad9
Abhishek Saini <abhisni@iitk.ac.in>
2018-05-22 20:50:56 +0530

    Create my_vim.sh

---
+++ my_vim.sh
@@ -0,0 +1,20 @@
+cd 
+git clone https://github.com/joshdick/onedark.vim
+cd .vim
+if [[ $(ls -d */ | grep "colors") == "" ]];then 
+  mkdir colors
+fi
+cd colors
+for x in ~/onedark.vim/colors/*;do
+  cp -r $x .
+done
+if [[ $(ls -d */ | grep "autoload") == " " ]];then
+  mkdir "autoload"
+fi
+for x in ~/onedark.vim/autoload/*;do
+  cp -r $x .
+done
+cat setup.txt >> ~/.vimrc
+echo "syntax on" >> ~/.vimrc
+echo "colorscheme onedark" >> ~/.vimrc
+vim
